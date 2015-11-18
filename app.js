36a5da233497af707e2fa66cec5da8bd4b512e6a
raphaelracine <raphael.racine@heig-vd.ch>
2015-11-18 16:22:32 +0100

    Navbar navigation OK (using angular ng-class)

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ require('./config/express')(app, config);
 
 var port = process.env.port || 3000;
 
-app.listen(port, function () {
+var server = app.listen(port, function () {
   console.log('Express server listening on port ' + port);
 });
 
 // Create a socket-io for communications with clients
-var socketio = require('socket.io')(require('http').createServer(app));
+var socketio = require('socket.io')(server);
 
-module.exports = socketio;
+socketio.on('connection', function() {
+  console.log("Salut");
+});
 
--- public/js/my_angular_module.js
+++ public/js/my_angular_module.js
@@ -1,48 +1,53 @@
-(function() {
-	
-    var module = angular.module('myApp', ['chart.js', 'btford.socket-io', 'ui.router'])
-    
-    module.config(function($stateProvider, $urlRouterProvider) {
-        
-        $urlRouterProvider.otherwise('welcome');
-      
-        $stateProvider.state('welcome', {
-          url:'/welcome',
-          templateUrl:'template/welcome'
-        });
-		
-		$stateProvider.state('audience', {
-          url:'/audience',
-          templateUrl:'template/audience'
-        });
-		
-		$stateProvider.state('board', {
-          url:'/board',
-          templateUrl:'template/board'
-        });
-		
-		$stateProvider.state('debug', {
-          url:'/debug',
-          templateUrl:'template/debug'
-        });
-		
-    });
-    
-	module.factory('mySocket', function(socketFactory) {
-		return socketFactory();
-	});
-	
-    module.factory('votesData', function(mySocket) {
-      
-        return {
-            labels:["Yes","No","I don't know"],
-            data:[[50,30,0]]          
-        };
-    });
-	
-	module.controller('BoardController', function($scope, votesData) {
-		$scope.labels = votesData.labels;
-		$scope.data = votesData.data;
-	});
-	
+  (function() {
+
+  var module = angular.module('myApp', ['chart.js', 'btford.socket-io', 'ui.router'])
+
+  module.config(function($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('welcome');
+
+      $stateProvider.state('welcome', {
+        url:'/welcome',
+        templateUrl:'template/welcome'
+      });
+
+      $stateProvider.state('audience', {
+        url:'/audience',
+        templateUrl:'template/audience'
+      });
+
+      $stateProvider.state('board', {
+        url:'/board',
+        templateUrl:'template/board'
+      });
+
+      $stateProvider.state('debug', {
+        url:'/debug',
+        templateUrl:'template/debug'
+      });
+
+  });
+
+  module.factory('mySocket', function(socketFactory) {
+      return socketFactory();
+  });
+
+  module.factory('votesData', function(mySocket) {      
+      return {
+          labels:["Yes","No","I don't know"],
+          data:[[50,30,0]]          
+      };
+  });
+
+  module.controller('BoardController', function($scope, votesData) {
+      $scope.labels = votesData.labels;
+      $scope.data = votesData.data;
+  });
+
+  module.controller('HeaderController', function($scope, $location) {
+	  $scope.isActive = function(viewLocation) {
+		  return viewLocation === $location.path();
+	  };
+  });
+
 })();
